2f12327c4a5ca3b23f34fe583c27bba8126d28e2
zergrael <zergrael@gmail.com>
2012-08-30 00:25:50 +0200

    0.6.3 - From 0.6.2 // + Smiley title in list // + Image link in shoutbox // ! Options tabs // + Macros // ! Strange backup behavior

--- userscript/release/FTDB_Shoutbox_Mod.meta.js
+++ userscript/release/FTDB_Shoutbox_Mod.meta.js
@@ -5,5 +5,5 @@
 // @include         *://*.frenchtorrentdb.com/?section=COMMUNAUTE
 // @downloadURL     https://thetabx.net/download/FTDB_Shoutbox_Mod.user.js
 // @updateURL       https://thetabx.net/download/FTDB_Shoutbox_Mod.meta.js
-// @version         0.6.2
+// @version         0.6.3
 // ==/UserScript==
